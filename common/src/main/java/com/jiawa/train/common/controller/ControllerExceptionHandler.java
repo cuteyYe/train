520e6f5a50f1f78c2df1d27b822c7bb686d11ee5
xiaoye <3456926514@qq.com>
2023-07-24 17:13:01 +0800

    4.18 使用自定义异常处理异常业务

--- common/src/main/java/com/jiawa/train/common/controller/ControllerExceptionHandler.java
+++ common/src/main/java/com/jiawa/train/common/controller/ControllerExceptionHandler.java
@@ -1,6 +1,7 @@
 package com.jiawa.train.common.controller;
 
 
+import com.jiawa.train.common.exception.BusinessException;
 import com.jiawa.train.common.resp.CommonResp;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -22,7 +23,7 @@ public class ControllerExceptionHandler {
      */
     @ExceptionHandler(value = Exception.class)
     @ResponseBody
-    public CommonResp exceptionHandler(Exception e) throws Exception {
+    public CommonResp exceptionHandler(Exception e)  {
         // LOG.info("seata全局事务ID: {}", RootContext.getXID());
         // // 如果是在一次全局事务里出异常了，就不要包装返回值，将异常抛给调用方，让调用方回滚事务
         // if (StrUtil.isNotBlank(RootContext.getXID())) {
@@ -31,10 +32,18 @@ public class ControllerExceptionHandler {
         CommonResp commonResp = new CommonResp();
         LOG.error("系统异常：", e);
         commonResp.setSuccess(false);
-//        commonResp.setMessage("系统出现异常，请联系管理员");
-        commonResp.setMessage(e.getMessage());
+        commonResp.setMessage("系统出现异常，请联系管理员");
         return commonResp;
     }
 
+    @ExceptionHandler(value = BusinessException.class)
+    @ResponseBody
+    public CommonResp exceptionHandler(BusinessException e)  {
+        CommonResp commonResp = new CommonResp();
+        LOG.error("业务异常：", e);
+        commonResp.setSuccess(false);
+        commonResp.setMessage(e.getE().getDesc());
+        return commonResp;
+    }
 
 }

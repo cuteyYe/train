743715d77f0c52d34e957fccf739e4d223b68d03
xiaoye <3456926514@qq.com>
2023-07-29 18:41:56 +0800

    7.9 集成PageHelper 实现sql分页功能

--- member/src/main/java/com/jiawa/train/member/service/PassengerService.java
+++ member/src/main/java/com/jiawa/train/member/service/PassengerService.java
@@ -3,6 +3,7 @@ package com.jiawa.train.member.service;
 import cn.hutool.core.bean.BeanUtil;
 import cn.hutool.core.date.DateTime;
 import cn.hutool.core.util.ObjectUtil;
+import com.github.pagehelper.PageHelper;
 import com.jiawa.train.common.context.LoginMemberContext;
 import com.jiawa.train.common.util.SnowUtil;
 import com.jiawa.train.member.domain.Passenger;
@@ -39,6 +40,8 @@ public class PassengerService {
         if(ObjectUtil.isNotNull(req.getMemberId())) {
             criteria.andMemberIdEqualTo(req.getMemberId());
         }
+        //分页语句
+        PageHelper.startPage(1,2);
         List<Passenger> passengerList = passengerMapper.selectByExample(passengerExample);
         return BeanUtil.copyToList(passengerList,PassengerQueryResp.class);
     }

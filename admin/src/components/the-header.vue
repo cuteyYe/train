f33776ba72fbf91c86e5da2a47182132d1add2d0
xiaoye <3456926514@qq.com>
2023-08-01 09:46:44 +0800

    9.3 修改admin和web模块去掉无用组件，修改logo区域

--- admin/src/components/the-header.vue
+++ admin/src/components/the-header.vue
@@ -1,11 +1,12 @@
 <template>
   <a-layout-header class="header">
-    <div class="logo" />
-    <div style="float: right;color: white">
-        您好:{{member.mobile}} &nbsp;
-      <router-link to="/login" style="color: white">
-        退出登录
+    <div class="logo">
+      <router-link to="/welcome">
+        希耶12306控台
       </router-link>
+    </div>
+    <div style="float: right;color: white">
+      欢迎使用管理控台
     </div>
     <a-menu
         v-model:selectedKeys="selectedKeys"
@@ -18,9 +19,9 @@
           <coffee-outlined /> &nbsp; 欢迎
         </router-link>
       </a-menu-item>
-      <a-menu-item key="/passenger">
-        <router-link to="/passenger">
-          <user-outlined /> &nbsp; 乘车人管理
+      <a-menu-item key="/about">
+        <router-link to="/about">
+          <user-outlined /> &nbsp; 关于
         </router-link>
       </a-menu-item>
     </a-menu>
@@ -31,13 +32,11 @@
 
 <script>
 import {defineComponent, ref,watch} from "vue";
-import store from "@/store"
 import router from "@/router";
 
 export default defineComponent({
   name:"this-header-view",
   setup() {
-    let member = store.state.member
     const  selectedKeys = ref([])
 
     watch(() => router.currentRoute.value.path,(newValue) => {
@@ -48,12 +47,17 @@ export default defineComponent({
 
     return {
       selectedKeys,
-      member
     };
   },
 });
 </script>
 
 <style scoped>
-
+.logo{
+  float: left;
+  height: 31px;
+  width: 150px;
+  color: white;
+  font-size: 20px;
+}
 </style>

b2c0a1f94e854102e4f88de16eed121ddd1a8a36
xiaoye <3456926514@qq.com>
2023-08-03 09:09:39 +0800

    9.15 为火车车站页面增加车次查询条件

--- business/src/main/java/com/jiawa/train/business/req/TrainStationQueryReq.java
+++ business/src/main/java/com/jiawa/train/business/req/TrainStationQueryReq.java
@@ -3,10 +3,22 @@ package com.jiawa.train.business.req;
 import com.jiawa.train.common.req.PageReq;
 
 public class TrainStationQueryReq extends PageReq {
+    private String trainCode;
+
+    public String getTrainCode() {
+        return trainCode;
+    }
 
     @Override
     public String toString() {
-        return "TrainStationQueryReq{" +
-                "} " + super.toString();
+        final StringBuffer sb = new StringBuffer("TrainStationQueryReq{");
+        sb.append("trainCode='").append(trainCode).append('\'');
+        sb.append('}');
+        return sb.toString();
     }
+
+    public void setTrainCode(String trainCode) {
+        this.trainCode = trainCode;
+    }
+
 }

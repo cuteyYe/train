00ae30ce4b2f29d7afd65afb2419cefa03c1f744
xiaoye <3456926514@qq.com>
2023-08-03 16:21:47 +0800

    10.6 禁止任务并发执行

--- batch/src/main/java/com/jiawa/train/batch/job/SpringBootTestJob.java
+++ batch/src/main/java/com/jiawa/train/batch/job/SpringBootTestJob.java
@@ -1,21 +1,18 @@
 package com.jiawa.train.batch.job;
 
-import org.springframework.scheduling.annotation.EnableScheduling;
-import org.springframework.stereotype.Component;
-
 /**
  * 适合单体应用，不适合集群
  * 无法实时更改定时任务状态和策略
  */
 
-@EnableScheduling  //必须加上这个注解
-@Component
-public class SpringBootTestJob {
-
-    //每5秒执行一次
+//@EnableScheduling  //必须加上这个注解
+//@Component
+//public class SpringBootTestJob {
+//
+//    //每5秒执行一次
 //    @Scheduled(cron = "0/5 * * * * ?")
-    private void test(){
-        //增加分布式锁解决集群问题
-        System.out.println("SpringBootTestJob Test");
-    }
-}
+//    private void test(){
+//        //增加分布式锁解决集群问题
+//        System.out.println("SpringBootTestJob Test");
+//    }
+//}
--- batch/src/main/java/com/jiawa/train/batch/job/TestJob.java
+++ batch/src/main/java/com/jiawa/train/batch/job/TestJob.java
@@ -1,14 +1,22 @@
 package com.jiawa.train.batch.job;
 
+import org.quartz.DisallowConcurrentExecution;
 import org.quartz.Job;
 import org.quartz.JobExecutionContext;
 import org.quartz.JobExecutionException;
 
+@DisallowConcurrentExecution //禁用并发执行
 public class TestJob implements Job {
 
 
     @Override
     public void execute(JobExecutionContext jobExecutionContext) throws JobExecutionException {
-        System.out.println("TestJob TEST");
+        System.out.println("TestJob TEST开始");
+        try {
+            Thread.sleep(3000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        System.out.println("TestJob TEST结束");
     }
 }

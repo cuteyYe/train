cb0dc7591284ba3374d23058b825f5b3c20073ce
xiaoye <3456926514@qq.com>
2023-08-03 09:18:44 +0800

    9.15 为火车车厢页面增加车次查询条件，注意null和""区别

--- business/src/main/java/com/jiawa/train/business/req/TrainCarriageQueryReq.java
+++ business/src/main/java/com/jiawa/train/business/req/TrainCarriageQueryReq.java
@@ -4,9 +4,22 @@ import com.jiawa.train.common.req.PageReq;
 
 public class TrainCarriageQueryReq extends PageReq {
 
+    private String trainCode;
+
     @Override
     public String toString() {
-        return "TrainCarriageQueryReq{" +
-                "} " + super.toString();
+        final StringBuffer sb = new StringBuffer("TrainCarriageQueryReq{");
+        sb.append("trainCode='").append(trainCode).append('\'');
+        sb.append('}');
+        return sb.toString();
     }
+
+    public String getTrainCode() {
+        return trainCode;
+    }
+
+    public void setTrainCode(String trainCode) {
+        this.trainCode = trainCode;
+    }
+
 }

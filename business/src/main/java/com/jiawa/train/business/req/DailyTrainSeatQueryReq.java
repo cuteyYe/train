6656f043f0e464dfaa83c820fbbb0137782c909a
xiaoye <3456926514@qq.com>
2023-08-04 10:50:55 +0800

    11.6 修改每日座位页面、列号显示 增加车次查询条件

--- business/src/main/java/com/jiawa/train/business/req/DailyTrainSeatQueryReq.java
+++ business/src/main/java/com/jiawa/train/business/req/DailyTrainSeatQueryReq.java
@@ -3,10 +3,22 @@ package com.jiawa.train.business.req;
 import com.jiawa.train.common.req.PageReq;
 
 public class DailyTrainSeatQueryReq extends PageReq {
+    private String trainCode;
 
     @Override
     public String toString() {
-        return "DailyTrainSeatQueryReq{" +
-                "} " + super.toString();
+        final StringBuffer sb = new StringBuffer("DailyTrainSeatQueryReq{");
+        sb.append("trainCode='").append(trainCode).append('\'');
+        sb.append('}');
+        return sb.toString();
     }
+
+    public String getTrainCode() {
+        return trainCode;
+    }
+
+    public void setTrainCode(String trainCode) {
+        this.trainCode = trainCode;
+    }
+
 }

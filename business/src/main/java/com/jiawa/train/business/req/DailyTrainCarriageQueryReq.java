21b4dd769159a622e09b10f6f8f9c43e30735194
xiaoye <3456926514@qq.com>
2023-08-04 10:33:57 +0800

    11.5 增加日期和车次查询条件

--- business/src/main/java/com/jiawa/train/business/req/DailyTrainCarriageQueryReq.java
+++ business/src/main/java/com/jiawa/train/business/req/DailyTrainCarriageQueryReq.java
@@ -1,12 +1,39 @@
 package com.jiawa.train.business.req;
 
 import com.jiawa.train.common.req.PageReq;
+import org.springframework.format.annotation.DateTimeFormat;
+
+import java.util.Date;
 
 public class DailyTrainCarriageQueryReq extends PageReq {
 
+    @DateTimeFormat(pattern = "yyyy-MM-dd")
+    private Date date;
+
     @Override
     public String toString() {
-        return "DailyTrainCarriageQueryReq{" +
-                "} " + super.toString();
+        final StringBuffer sb = new StringBuffer("DailyTrainCarriageQueryReq{");
+        sb.append("date=").append(date);
+        sb.append(", trainCode='").append(trainCode).append('\'');
+        sb.append('}');
+        return sb.toString();
+    }
+
+    private String trainCode;
+
+    public Date getDate() {
+        return date;
+    }
+
+    public void setDate(Date date) {
+        this.date = date;
+    }
+
+    public String getTrainCode() {
+        return trainCode;
+    }
+
+    public void setTrainCode(String trainCode) {
+        this.trainCode = trainCode;
     }
 }

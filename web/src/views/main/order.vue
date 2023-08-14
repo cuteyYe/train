1aa117b0fa8a07dd06b75b7a4f9d39061130a90f
xiaoye <3456926514@qq.com>
2023-08-14 14:09:45 +0800

    12.8 订单页面,为勾选的乘客构造购票数据

--- web/src/views/main/order.vue
+++ web/src/views/main/order.vue
@@ -21,10 +21,12 @@
   <a-checkbox-group v-model:value="passengerChecks" :options="passengerOptions" />
   <br/>
   选中的乘客:{{passengerChecks}}
+  <br/>
+  购票列表:{{tickets}}
 </template>
 
 <script>
-import {defineComponent, onMounted, ref} from "vue";
+import {defineComponent, onMounted, ref, watch} from "vue";
 import axios from "axios";
 export default defineComponent({
   name:"order-view",
@@ -60,6 +62,32 @@ export default defineComponent({
       }
     }
     console.log("本车次提供的座位：", seatTypes)
+    // 购票列表，用于界面展示，并传递到后端接口，用来描述：哪个乘客购买什么座位的票
+    // {
+    //   passengerId: 123,
+    //   passengerType: "1",
+    //   passengerName: "张三",
+    //   passengerIdCard: "12323132132",
+    //   seatTypeCode: "1",
+    //   seat: "C1"
+    // }
+    const tickets = ref([]);
+
+    // 勾选或去掉某个乘客时，在购票列表中加上或去掉一张表
+    watch(() => passengerChecks.value, (newVal, oldVal)=>{
+      console.log("勾选乘客发生变化", newVal, oldVal)
+      // 每次有变化时，把购票列表清空，重新构造列表
+      tickets.value = [];
+      passengerChecks.value.forEach((item) => tickets.value.push({
+        passengerId: item.id,
+        passengerType: item.type,
+        seatTypeCode: seatTypes[0].code,
+        passengerName: item.name,
+        passengerIdCard: item.idCard
+      }))
+    }, {immediate: true});
+
+
 
     const handleQueryPassenger = () => {
       axios.get("/member/passenger/query-mine").then((response) => {
@@ -85,7 +113,8 @@ export default defineComponent({
       seatTypes,
       passengers,
       passengerOptions,
-      passengerChecks
+      passengerChecks,
+      tickets
     }
   }
 })

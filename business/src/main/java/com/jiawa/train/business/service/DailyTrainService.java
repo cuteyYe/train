13c9883d4ede212eaa1671e1512c4dd7935b1d23
xiaoye <3456926514@qq.com>
2023-08-30 08:41:00 +0800

    18.3 初始化车次信息时初始化秒杀令牌信息

--- business/src/main/java/com/jiawa/train/business/service/DailyTrainService.java
+++ business/src/main/java/com/jiawa/train/business/service/DailyTrainService.java
@@ -3,6 +3,7 @@ package com.jiawa.train.business.service;
 import cn.hutool.core.bean.BeanUtil;
 import cn.hutool.core.collection.CollUtil;
 import cn.hutool.core.date.DateTime;
+import cn.hutool.core.date.DateUtil;
 import cn.hutool.core.util.ObjectUtil;
 import com.github.pagehelper.PageHelper;
 import com.github.pagehelper.PageInfo;
@@ -46,6 +47,9 @@ public class DailyTrainService {
 
     @Resource
     private DailyTrainTicketService dailyTrainTicketService;
+
+    @Resource
+    private SkTokenService skTokenService;
 
     public void save(DailyTrainSaveReq req) {
         DateTime now = DateTime.now();
@@ -138,5 +142,10 @@ public class DailyTrainService {
 
         //生成该车次的余票数据
         dailyTrainTicketService.genDaily(dailyTrain,date,train.getCode());
+
+        //生成令牌余量数据
+        skTokenService.genDaily(date,train.getCode());
+
+        LOG.info("生成日期【{}】车次【{}】的信息结束", DateUtil.formatDate(date), train.getCode());
     }
 }
